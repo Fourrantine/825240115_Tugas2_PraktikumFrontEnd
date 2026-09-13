dbf0cb4e0272154758658be6b1f08d1c16ad5fbe
Fourrantine <cks.jansen2@gmail.com>
2026-09-13 18:47:51 +0700

    825240115_Tugas2_PraktikumFrontEnd

---
+++ script.js
@@ -0,0 +1,8 @@
+function tampilkanData() {
+    document.getElementById("dataMahasiswa").innerHTML =
+        "Nama: Jansen Fourrantine<br>NIM: 123456789";
+}
+
+function hapusData() {
+    document.getElementById("dataMahasiswa").innerHTML = "";
+}
